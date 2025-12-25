82a63cf61f674ae395d517a92f3c3c8c72484eac
Adi <yos.evony2022@gmail.com>
2025-12-26 02:41:45 +0700

    fix

--- frontend/assets/js/api.js
+++ frontend/assets/js/api.js
@@ -1,12 +1,17 @@
 const api = axios.create({
-  baseURL: "http://localhost:5000/api",
-  headers: { "Content-Type": "application/json" },
+  baseURL: "https://app-lestahfizh.vercel.app/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-  return config;
-});
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => Promise.reject(error)
+);
