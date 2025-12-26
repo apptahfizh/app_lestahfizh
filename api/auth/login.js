677da603452aba8ee0128467062a482d3c26213e
Adi <yos.evony2022@gmail.com>
2025-12-26 12:45:07 +0700

    fix: remove extra vercel api functions, keep single api/index.js

--- api/auth/login.js
+++
@@ -1,5 +0,0 @@
-const app = require("../_app");
-
-module.exports = (req, res) => {
-  return app(req, res);
-};
--- api/ortu/ortu-dashboard.js
+++
@@ -1,2 +0,0 @@
-const app = require("../_app");
-module.exports = (req, res) => app(req, res);
